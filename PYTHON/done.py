d278b1cf398020eaac471120f5f22ae84933c650
mxis34 <natakongame@gmail.com>
2025-01-10 20:08:39 +0700

    save

--- PYTHON/done.py
+++ PYTHON/done.py
@@ -1,3 +1,15 @@
-print("num 1")
-print("num 2")
-print("num 3")
+done = True
+num = int(input("Enter number : "))
+while done:
+    # num = int(input("Enter number : "))
+    if num <= 8 and num > -1:
+        num + 1
+        print(num)
+    else:
+        print("your answer is\'t correct")
+        done = False
+    
+    print("number is ",num,)
+    num += 1
+    
+    
