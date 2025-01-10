570fb9d4b366df4c21b69d8ad615a19a37a3453c
mxis34 <natakongame@gmail.com>
2025-01-10 20:25:16 +0700

    save

--- PYTHON/done.py
+++ PYTHON/done.py
@@ -12,4 +12,19 @@ while done:
     print("number is ",num,)
     num += 1
     
+print()
+print("main menu")
+menu = "1. print\n"+"2. table\n"
+
+print(menu)
+choice = int(input("Choice 1 or 2 : "))
+if choice == 1:
+    print("good Job !!")
+elif choice == 2:
+    table = " ll \n"
+    table += " ll \n"
+    table += " lllllll\n"
+    table += " ll   ll\n"
+    table += " ll   ll\n"
     
+    print(table)
