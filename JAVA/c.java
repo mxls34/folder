20a5a14db3447001afef3d4fdb1994aab60edeb3
mxls34 <natakongame@gmail.com>
2025-01-12 14:32:16 +0700

    java

--- JAVA/c.java
+++ JAVA/c.java
@@ -3,22 +3,23 @@ import javax.swing.JOptionPane;
 
 public class c {
     public static void main(String[] args) {
-        System.out.println("hello World");
-        String strnum = JOptionPane.showInputDialog("Enter integer number : ");
-        int num = Integer.parseInt(strnum);
-        
-        if (num >= 0){
-            JOptionPane.showMessageDialog(null, "Your number is " + strnum, "ANSWER", JOptionPane.INFORMATION_MESSAGE);
-        }else if (num == 0){
-            JOptionPane.showMessageDialog(null, "Your number is " + strnum,"ANSWER",JOptionPane.INFORMATION_MESSAGE);          
-        }else{
-            JOptionPane.showMessageDialog(null, "Your number is " + strnum,"ANSWER",JOptionPane.WARNING_MESSAGE);
-            JOptionPane.showMessageDialog(null, "HAPPY NEW YEAR","1 January 2025",JOptionPane.INFORMATION_MESSAGE);
-            JOptionPane.showMessageDialog(null, " Hi ", "Say", JOptionPane.INFORMATION_MESSAGE);
+
+        boolean done = true;
+        while (done) { 
+            String strnum = JOptionPane.showInputDialog("Enter integer number : ");
+            int num = Integer.parseInt(strnum);
+            
+            if (num >= 0){
+                JOptionPane.showMessageDialog(null, "Your number is " + strnum, "ANSWER", JOptionPane.INFORMATION_MESSAGE);
+            }else if (num == 0){
+                JOptionPane.showMessageDialog(null, "Your number is " + strnum,"ANSWER",JOptionPane.INFORMATION_MESSAGE);          
+            }else{
+                JOptionPane.showMessageDialog(null, "Your number is " + strnum,"ANSWER",JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null, "HAPPY NEW YEAR","1 January 2025",JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, " Hi ", "Say", JOptionPane.INFORMATION_MESSAGE);
+            }    
         }
         System.exit(0);
-
-
-
+        
     }
 }
