d13f80e7925137af68b1e683144844d0af6a9cfa
mxls34 <natakongame@gmail.com>
2025-01-12 14:34:46 +0700

    java

--- JAVA/c.java
+++ JAVA/c.java
@@ -17,9 +17,10 @@ public class c {
                 JOptionPane.showMessageDialog(null, "Your number is " + strnum,"ANSWER",JOptionPane.WARNING_MESSAGE);
                 JOptionPane.showMessageDialog(null, "HAPPY NEW YEAR","1 January 2025",JOptionPane.INFORMATION_MESSAGE);
                 JOptionPane.showMessageDialog(null, " Hi ", "Say", JOptionPane.INFORMATION_MESSAGE);
+                done = false;
             }    
         }
         System.exit(0);
-        
+
     }
 }
